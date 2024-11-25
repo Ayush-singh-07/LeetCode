74c15135940bbd4e05e4962eb95034aee169c246
Ayush Singh <78557460+Ayush-singh-07@users.noreply.github.com>
2024-11-25 22:52:00 +0530

    LC-valid-palindrome [Runtime: N/A; Memory: N/A]

---
+++ string/valid-palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+    string rev(string str){
+        int start = 0, end = str.size()-1;
+        while(start < end){
+            swap(str[start], str[end]);
+            start++;
+            end--;
+        }
+        return str;
+    }
+
+    bool isPalindrome(string s) {
+        string plain = "";
+        for(auto i: s){
+            if(isalpha(i))
+                plain+=tolower(i);
+        }
+        return plain == rev(plain);
+
+    }
+};
