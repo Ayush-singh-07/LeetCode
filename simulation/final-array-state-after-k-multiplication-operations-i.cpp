95aeea558361cc199b11476ddb367ead11b72126
Ayush Singh <78557460+Ayush-singh-07@users.noreply.github.com>
2025-06-08 18:31:27 +0530

    LC-final-array-state-after-k-multiplication-operations-i [Runtime: 1 ms; Memory: 27 MB]

---
+++ simulation/final-array-state-after-k-multiplication-operations-i.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+
+        int to_replace = -1;   // value
+        int replace_from = -1; // index
+        int mini = INT_MAX;
+
+        for (int j = 0; j < k+1; j++) {
+            if (to_replace != -1 && replace_from != -1) {
+                to_replace *= multiplier;
+                nums[replace_from] = to_replace;
+                to_replace = replace_from = -1;
+                mini = INT_MAX;
+            }
+            for (int i = 0; i < nums.size(); i++) {
+                if (mini > nums[i]) {
+                    mini = to_replace = nums[i];
+                    replace_from = i;
+                }
+            }
+        }
+
+        return nums;
+    }
+};
