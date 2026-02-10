118acec4d87b80de18eaa5146b3bb5fee3ec6341
Ayush Singh <78557460+Ayush-singh-07@users.noreply.github.com>
2026-02-10 12:58:16 +0530

    LC-set-mismatch [Runtime: 33 ms; Memory: 34.7 MB]

--- sorting/set-mismatch.cpp
+++ sorting/set-mismatch.cpp
@@ -2,15 +2,23 @@ class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
         unordered_map <int, int> mp;
+        int reapeated = -1;
+        int missing = -1;
 
         for(int i: nums){
             mp[i]++;
         }
 
-        for(auto i: mp){
-            cout<<i.first<<" -> "<<i.second<<"\n";
+        for(int i=1; i<=nums.size(); i++){
+            
+            if(mp.find(i) != mp.end()){
+                if(mp[i] > 1) reapeated = i;
+            }
+            else{
+                missing = i;
+            }
+                
         }
-
-        return nums;
+        return {reapeated,missing};
     }
 };
